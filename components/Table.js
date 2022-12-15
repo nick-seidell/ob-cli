b837b5ef064e907d6f9ddf567495c7e0253b1c47
nick <nick.seidell@ns-001.lan>
2022-12-15 13:08:04 -0500

    Initial websocket implementation

--- components/Table.js
+++ components/Table.js
@@ -1,15 +1,74 @@
 const React = require('react');
 const { useState, useEffect } = React;
 const { Box, Text, Newline } = require('ink');
-const axios = require('axios');
-
-const url = 'https://api.gemini.com/v1/pubticker/btcusd'
+const WebSocket = require('ws');
 
 const Table = () => {
 
-  const [data, setData] = useState([]);
+  const [data, setData] = useState({});
+  const [bid, setBids] = useState({});
+  const [ask, setAsks] = useState({})
+  const [webSocketReady, setWebSocketReady] = useState(false);
+    
+  const [ws, setWS] = useState(new WebSocket('wss://api.gemini.com/v1/marketdata/BTCUSD?top_of_book=true&asks=false'));
 
   useEffect(()=>{
+
+    ws.onopen = function (event) {
+      setWebSocketReady(true);
+      console.log('Connection established.');
+    };
+
+    ws.onclose = function (event) {
+      setWebSocketReady(false);
+      setTimeout(() => {
+        setWS(new WebSocket('wss://api.gemini.com/v1/marketdata/BCTUSD?top_of_book=true&asks=false'))
+      }, 1000);
+    };
+
+    ws.onerror = function (err) {
+      console.log('Socket encountered error: ' + err.message, 'Closing socket.');
+      ws.close();
+    }
+
+    ws.onmessage = function (event) {
+      const json = JSON.parse(event.data);
+      setData(json.events[0]);
+    };
+
+    return () => {
+      ws.close
+    };
+
+  },[ws]);
+
+  if (!webSocketReady) {
+    return (
+      <Box borderStyle='single' padding={2} flexDirection='column'>
+              <Box>
+                  <Box width='25%'><Text>Socket not ready</Text></Box>
+              </Box>
+      </Box>
+    )
+  } else if (Object.keys(data).length == 0){
+    return (
+      <Box borderStyle='single' padding={2} flexDirection='column'>
+              <Box>
+                  <Box width='25%'><Text>Waiting for data from server...</Text></Box>
+              </Box>
+      </Box>
+    )
+  } else {
+    return (
+      <Box borderStyle='single' padding={2} flexDirection='column'>
+              <Box>
+                  <Box width='25%'><Text>{data.price}</Text></Box>
+              </Box>
+      </Box>
+    )
+  }
+
+  /*useEffect(()=>{
     const interval = setInterval(()=>{
       axios.get(url)
       .then(response => setData(response.data))
@@ -25,16 +84,16 @@ const Table = () => {
             </Box>
             <Newline/>
             <Box>
-                <Box width='25%'><Text color='#fd79a8'>{data.ask}</Text></Box>
+                <Box width='25%'><Text color='#fd79a8'>ASK</Text></Box>
             </Box>
             <Box>
-                <Box width='25%'><Text>{'∆ ' + (data.ask - data.bid).toLocaleString()}</Text></Box>
+                <Box width='25%'><Text>Placeholder</Text></Box>
             </Box>
             <Box>
-                <Box width='25%'><Text color='#c3e88d'>{data.bid}</Text></Box>
+                <Box width='25%'><Text color='#c3e88d'>BID</Text></Box>
             </Box>
     </Box>
-  )
+  )*/
 }
 
 module.exports = Table;
